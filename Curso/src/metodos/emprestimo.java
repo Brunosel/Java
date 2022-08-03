0534c523462557854a9cb345eb6d15b1e75b66e2
Brunosel <brunosel@hotmail.com>
2022-08-03 18:17:49 -0300

    metodos improve switch

--- Curso/src/metodos/emprestimo.java
+++ Curso/src/metodos/emprestimo.java
@@ -10,13 +10,12 @@ public class emprestimo {
 		double a = scan.nextDouble();
 		System.out.println("Digite o valor de parcelas: ");
 		int b = scan.nextInt();
-		System.out.println(emprestimo(a, b));
+		System.out.println(calculaEmprestimo(a, b));
 	}
 	static Scanner scan = new Scanner(System.in);
 	
-	static double emprestimo(double price, int installments) {
+	static double calculaEmprestimo(double price, int installments) {
 		double rate = getRate(installments);
-		System.out.println((price / installments) * rate);
 		return installments * ((price / installments) * rate);	
 	}
 	
@@ -29,12 +28,9 @@ public class emprestimo {
 		case 3: 
 			return 1.3;
 		case 4: 
-			return 1.4;		
-		}
-		if (installments > 4) {
+			return 1.4;
+		default:
 			return 1.5;
-		} else {
-			return 1;
 		}
 	}
 
